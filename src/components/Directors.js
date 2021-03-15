f92a94d8a03f6460ac8be82d1cf9c6d5a4d2f2a2
github username <rvtingling@yahoo.com>
2021-03-15 11:43:44 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,8 +4,18 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+    <h1>Directors Page</h1>
+    {directors.map(director => (
+      <div>
+        {director.name}
+        <ul>
+          {director.movies.map(movie => (
+            <li>{movie}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </div>
   );
 }
 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,8 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
-    </div>
+    <h1>Movies Page</h1>
+    {movies.map(movie => (
+      <div>
+        <h2>{movie.title}</h2>
+        <h3>Time: {movie.time}</h3>
+        {movie.metascore ? <h3>metascore: {movie.metascore}</h3> : ""}
+        <ul>
+          {movie.genres.map(genre => (
+            <li>{genre}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </div>
   );
 };
 
